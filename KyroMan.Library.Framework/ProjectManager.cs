e1b8dd052ab0e2012edaf482c75f29c5b8965bfb
gowra <rajgowtham@live.com>
2022-07-02 17:10:54 +0530

    Created ProjectManager as an entry point for the project management workflow

---
+++ KyroMan.Library.Framework/ProjectManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using KyroMan.Library.Framework.Model;
+
+namespace KyroMan.Library.Framework
+{
+    public class ProjectManager
+    {
+        private readonly HashSet<Project> projects;
+
+        public static ProjectManager Default = new ProjectManager();
+
+        private ProjectManager()
+        {
+            this.projects = new HashSet<Project>();
+        }
+
+        public Project CreateProject(string name, int duration) => throw new NotImplementedException();
+
+        public void RemoveProject(string name) => throw new NotImplementedException();
+    }
+}
