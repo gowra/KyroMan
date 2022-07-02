0b69d7208cdb3a90153d1df854071d5f440c1c64
gowra <rajgowtham@live.com>
2022-07-02 17:25:55 +0530

    Added a test project to demonstrate the usage of the library

---
+++ KryoMan.Library.Framework.Tests/ProjectManagerTests.cs
@@ -0,0 +1,56 @@
+﻿using KyroMan.Library.Framework;
+using KyroMan.Library.Framework.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KryoMan.Library.Framework.Tests
+{
+    [TestClass]
+    public class ProjectManagerTests
+    {
+        [TestMethod]
+        public void CheckFeasibility()
+        {
+            var project = ProjectManager.Default.CreateProject("Test", 30);
+            var manager = project.Users.AddUser("Manager", Role.Any);
+
+            var dev1 = project.Users.AddUser("Dev 1", Role.Developer);
+            var dev2 = project.Users.AddUser("Dev 2", Role.Developer);
+            var qa1 = project.Users.AddUser("QA 1", Role.Tester);
+            var devops = project.Users.AddUser("Devops 1", Role.DevOps);
+
+            var resource1 = project.Resources.AddResource("Resource 1");
+
+            var feature1 = project.Tasks.CreateTask("Feature 1", "", manager);
+            feature1.Estimate = 3;
+            feature1.RequiredRole = Role.Developer;
+
+            var feature2 = project.Tasks.CreateTask("Feature 2", "", manager);
+            feature2.Estimate = 5;
+            feature2.RequiredRole = Role.Developer;
+            feature2.RequiredResource = resource1;
+
+            var feature3 = project.Tasks.CreateTask("Feature 2 test", "", manager);
+            feature3.Estimate = 5;
+            feature3.RequiredRole = Role.Tester;
+            feature3.RequiredResource = resource1;
+
+            var installerTask = project.Tasks.CreateTask("Installer", "", manager);
+            installerTask.Estimate = 3;
+            installerTask.RequiredRole = Role.DevOps;
+
+            project.Scheduler.AssignTask(feature1, dev1);
+            
+            project.Scheduler.AssignTask(feature2, dev2);
+            project.Scheduler.AllocateResource(feature2, resource1);
+            
+            project.Scheduler.AssignTask(feature3, qa1);
+
+            project.Scheduler.AssignTask(installerTask, devops);
+
+            var estimate = project.Scheduler.GetEstimatedDuration();
+
+            Assert.IsTrue(estimate <= project.DurationInDays);
+        }
+    }
+}
---
+++ KryoMan.Library.Framework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KryoMan.Library.Framework.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Inc.")]
+[assembly: AssemblyProduct("KryoMan.Library.Framework.Tests")]
+[assembly: AssemblyCopyright("Copyright © Trimble Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("656e70c2-78fa-495b-9ca3-f8926b3cea55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
